507cf796c5b8fc720e77783d89621fae485fd2ff
namdp <ealflm@gmail.com>
2021-09-20 23:17:05 +0700

    Add dashboard demo

---
+++ WebApp/dashboard/src/app/services/users.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+
+  private basePath = 'https://booking-yacht-dev.eastasia.cloudapp.azure.com/api/member';
+  constructor(private http: HttpClient) {}
+  
+  public getUsers(): Observable<any> {
+    return this.http.get(this.basePath);
+  }
+}
