ec9b8a9279bb577e9398bcd6983dc8af5b65788a
Sang <sadwindsang@gmail.com>
2021-09-26 19:13:37 +0700

    fix bug reload angular

--- WebApp/booking-yacht/apps/booking-yacht/src/app/pages/pages-routing.module.ts
+++ WebApp/booking-yacht/apps/booking-yacht/src/app/pages/pages-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     path: '',
     component: PagesComponent,
     children: [
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'dashboard', component: HomePagesComponent },
       { path: 'manage-place-type', component: ManagePlaceComponent },
     ],
