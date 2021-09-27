ddfab04918cd62ae02ebed1fa60aae0b290f5d7c
DatNLM <dauphaidat@gmail.com>
2021-09-28 00:48:14 +0700

    update main screen

--- Mobile/owners_yacht/lib/screens/main_screen.dart
+++ Mobile/owners_yacht/lib/screens/main_screen.dart
@@ -14,48 +14,7 @@ class _MainScreenState extends State<MainScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('My Yacht'),
-        // actions: <Widget>[
-        //   PopupMenuButton(
-        //     onSelected: (FilterOptions selectedValue) {
-        //       setState(() {
-        //         if (selectedValue == FilterOptions.Favorites) {
-        //           _showOnlyFavorites = true;
-        //         } else {
-        //           _showOnlyFavorites = false;
-        //         }
-        //       });
-        //     },
-        //     icon: Icon(
-        //       Icons.more_vert,
-        //     ),
-        //     itemBuilder: (_) => [
-        //           PopupMenuItem(
-        //             child: Text('Only Favorites'),
-        //             value: FilterOptions.Favorites,
-        //           ),
-        //           PopupMenuItem(
-        //             child: Text('Show All'),
-        //             value: FilterOptions.All,
-        //           ),
-        //         ],
-        //   ),
-        //   Consumer<Cart>(
-        //     builder: (_, cart, ch) => Badge(
-        //           child: ch,
-        //           value: cart.itemCount.toString(),
-        //         ),
-        //     child: IconButton(
-        //       icon: Icon(
-        //         Icons.shopping_cart,
-        //       ),
-        //       onPressed: () {
-        //         Navigator.of(context).pushNamed(CartScreen.routeName);
-        //       },
-        //     ),
-        //   ),
-        // ],
       ),
-      // drawer: AppDrawer(),
       body: YachtGrid(),
     );
     }
