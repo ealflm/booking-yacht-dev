310b2ae4e98a987c411bdf79c35dc6340bc3854c
visualgianguyen <visualgianguyen@gmail.com>
2021-09-29 11:29:22 +0700

    Fix Business api

--- WebAPI/BookingYacht/BookingYacht.Business/SearchModels/BusinessSearchModel.cs
+++ WebAPI/BookingYacht/BookingYacht.Business/SearchModels/BusinessSearchModel.cs
@@ -12,6 +12,8 @@ namespace BookingYacht.Business.SearchModels
         public string Phone { get; set; }
         public string EmailAddress { get; set; }
         public string Address { get; set; }
-        public int Status { get; set; }
+        public Status Status { get; set; }
+        public int Page { get; set; }
+        public int AmountItem { get; set; }
     }
 }
