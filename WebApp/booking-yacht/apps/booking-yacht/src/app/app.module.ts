8cb289b73d8fbc4d69d13b9ff2c5233d70dc38ab
Sang <sadwindsang@gmail.com>
2021-09-30 15:02:14 +0700

    Login Username with token

--- WebApp/booking-yacht/apps/booking-yacht/src/app/app.module.ts
+++ WebApp/booking-yacht/apps/booking-yacht/src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { BusinessAccountService } from './services/business-account.service';
 import { AuthModule } from './auth/auth.module';
 import { PrimengModule } from './primeng/primeng/primeng.module';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -8,22 +7,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { PagesComponent } from './pages/pages.component';
-import { LoginComponent } from './auth/login/login.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // import { MaterialModule } from './material/material.module';
-import { ComponentsModule } from './components/components.module';
-import { ManagePlaceComponent } from './pages/manage-place/manage-place.component';
-
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 //firebase services
-
-import { CommonModule } from '@angular/common';
+import { ComponentsModule } from './components/components.module';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { JwtInterceptor } from './auth/jwt.interceptor';
 import { AccountBusinessFormComponent } from './pages/home-pages/account-business-form/account-business-form.component';
+import { ManagePlaceComponent } from './pages/manage-place/manage-place.component';
+import { PagesComponent } from './pages/pages.component';
+import { LoginComponent } from './auth/login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -49,7 +47,7 @@ import { AccountBusinessFormComponent } from './pages/home-pages/account-busines
   providers: [
     MessageService,
     ConfirmationService,
-    // { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
   ],
   bootstrap: [AppComponent],
   exports: [],
--- WebApp/booking-yacht/apps/booking-yacht/src/app/auth/login/login.component.ts
+++ WebApp/booking-yacht/apps/booking-yacht/src/app/auth/login/login.component.ts
@@ -1,3 +1,4 @@
+import { UsersService } from './../../services/users.service';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import {
@@ -7,6 +8,7 @@ import {
 } from 'angularx-social-login';
 import { AuthService } from '../auth.service';
 import { LocalStorageService } from '../localstorage.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'booking-yacht-login',
@@ -17,19 +19,46 @@ export class LoginComponent implements OnInit {
   socialUser?: SocialUser;
   isLoggedin?: boolean;
   response: any;
+  userForm!: FormGroup;
+  user: any;
+  isSubmit = false;
   constructor(
     private router: Router,
     private localStorageService: LocalStorageService,
-    private authService: AuthService
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private userService: UsersService
   ) {}
 
   ngOnInit(): void {
     if (this.localStorageService.getToken()) {
       this.router.navigate(['/dashboard']);
     }
+    this._initForm();
   }
+
+  private _initForm() {
+    this.userForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+  onSignIn() {
+    this.isSubmit = true;
+    this.userService
+      .signIn(this.usersForm.email.value, this.usersForm.password.value)
+      .subscribe((res) => {
+        this.localStorageService.setToken(res);
+        this.router.navigate(['dashboard']);
+      });
+  }
+
   loginWithGoogle(): void {
     this.authService.googleLogin();
   }
   logOut(): void {}
+
+  get usersForm() {
+    return this.userForm.controls;
+  }
 }
