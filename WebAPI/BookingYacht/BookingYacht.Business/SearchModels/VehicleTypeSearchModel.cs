1d5dee9d48cce5a12a77e1dae1128dd6cb179967
sumo6842 <sumo6842@gmail.com>
2021-10-08 08:54:47 +0700

    bug vehicle type

--- WebAPI/BookingYacht/BookingYacht.Business/SearchModels/VehicleTypeSearchModel.cs
+++ WebAPI/BookingYacht/BookingYacht.Business/SearchModels/VehicleTypeSearchModel.cs
@@ -4,10 +4,11 @@ namespace BookingYacht.Business.SearchModels
 {
     public class VehicleTypeSearchModel
     {
-        public Guid? Id { get; set; }
         public string Name { get; set; }
         public int? Status { get; set; }
         
-        public int Paging { get; set; }
+        public int Page { get; set; }
+        public int AmountItem { get; set; }
+        
     }
 }
