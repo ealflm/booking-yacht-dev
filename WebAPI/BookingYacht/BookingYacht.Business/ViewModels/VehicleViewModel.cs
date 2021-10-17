30ce63a3ac3c24901856e664c00818bfc650ef19
BasicZero <visualgianguyen@gmail.com>
2021-10-17 23:40:58 +0700

    update vehicle

--- WebAPI/BookingYacht/BookingYacht.Business/ViewModels/VehicleViewModel.cs
+++ WebAPI/BookingYacht/BookingYacht.Business/ViewModels/VehicleViewModel.cs
@@ -6,6 +6,9 @@ namespace BookingYacht.Business.ViewModels
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public string RegistrationNumber { get; set; }
+        public int? YearOfManufacture { get; set; }
+        public string WhereProduction { get; set; }
         public int Seat { get; set; }
         public string Descriptions { get; set; }
         public Guid IdVehicleType { get; set; }
