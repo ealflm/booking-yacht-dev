92d66840909a5063810b4473f1016faebc98677f
BasicZero <visualgianguyen@gmail.com>
2021-10-18 17:00:43 +0700

    Add vnpay

---
+++ WebAPI/BookingYacht/BookingYacht.Business/VNPay/OrderRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingYacht.Business.VNPay
+{
+    public class OrderRequest
+    {
+        public string Ip { get; set; }
+        public long Amount { get; set; }
+    }
+}
