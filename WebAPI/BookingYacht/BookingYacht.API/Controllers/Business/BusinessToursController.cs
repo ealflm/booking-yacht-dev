37e4aad8a4837b894d90bc6a71c379c8b69d6a5e
BasicZero <visualgianguyen@gmail.com>
2021-10-25 22:13:06 +0700

    add businessTour api

---
+++ WebAPI/BookingYacht/BookingYacht.API/Controllers/Business/BusinessToursController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using BookingYacht.Business.Interfaces.Admin;
+using BookingYacht.Business.SearchModels;
+using BookingYacht.Business.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingYacht.API.Controllers.Business
+{
+    [Route(BusinessRoute)]
+    [ApiController]
+    [Authorize]
+    [ApiExplorerSettings(GroupName = Role)]
+    public class BusinessToursController : BaseBusinessController
+    {
+        private readonly IBusinessTourService _service;
+
+        public BusinessToursController(IBusinessTourService service)
+        {
+            _service = service;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] BusinessTourSearchModel model)
+        {
+            var result = await _service.SearchAgenciesNavigation(model);
+            return Success(result);
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var result = await _service.GetBusinessTourNavigation(id);
+            return Success(result);
+        }
+
+        
+    }
+}
