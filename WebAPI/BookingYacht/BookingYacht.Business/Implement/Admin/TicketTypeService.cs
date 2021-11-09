f0b15d96c32ff182593e45054f6b9077b862c4ff
sumo6842 <sumo6842@gmail.com>
2021-11-09 08:59:28 +0700

    dummy bug

--- WebAPI/BookingYacht/BookingYacht.Business/Implement/Admin/TicketTypeService.cs
+++ WebAPI/BookingYacht/BookingYacht.Business/Implement/Admin/TicketTypeService.cs
@@ -37,10 +37,10 @@ namespace BookingYacht.Business.Implement.Admin
             var ticketTypes = await _unitOfWork.Context().TicketTypes
                 .Include(x => x.IdBusinessTourNavigation)
                 .Where(x => model.Status == Status.ALL | x.Status == (int)model.Status)
-                .Where(x => x.ServiceFeePercentage > 1.0 & x.ServiceFeePercentage < 10.0 )
+                // .Where(x => x.ServiceFeePercentage > 1.0 & x.ServiceFeePercentage < 10.0 )
                 .OrderBy(x => x.Status)
                 .Skip(model.AmountItem * ((model.Page != 0) ? (model.Page - 1) : model.Page))
-                .Take((model.Page != 0) ? model.AmountItem : _unitOfWork.TicketRepository.Query().Count())
+                .Take((model.Page != 0) ? model.AmountItem : _unitOfWork.TicketTypeRepository.Query().Count())
                 .ToListAsync();
             return ticketTypes;
         }
